d4d5a5ae4697baa1bd4435db4675dbc9463b2048
brianhsieh01 <chyuanxie@gmail.com>
2024-05-17 01:34:04 +0000

    feat: update login api

--- main/apps/user/api/views.py
+++ main/apps/user/api/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from main.apps.user.models import CustomUser 
+from main.apps.profile.models import Profile
+from main.apps.profile.serializers.serializer import ProfileDetailSerializer
 import json
 
 
@@ -12,18 +14,34 @@ def login_view(request):
     password = data.get('password')
     
     user = CustomUser.objects.filter(email=email).first()
-    if user is not None:
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return JsonResponse({'status': 'success', 'message': 'Login successful'}, status=200)
-            else:
-                return JsonResponse({'status': 'error', 'message': 'Account is disabled'}, status=401)
-        else:
-            return JsonResponse({'status': 'error', 'message': 'Password is incorrect'}, status=401)
-    else:
-        # User does not exist, create a new one
+    if user is None:
         new_user = CustomUser.objects.create_user(email=email, password=password, is_wallet_connected=False)
         login(request, new_user)
-        return JsonResponse({'status': 'success', 'message': 'User created and logged in successfully'}, status=200)
+        return JsonResponse({
+            'status': 'success',
+            'message': 'User created and logged in successfully',
+            'data': {'address': new_user.wallet_address}
+        }, status=200)
+
+    user = authenticate(request, username=email, password=password)
+    if user is None:
+        return JsonResponse({'status': 'error', 'message': 'Password is incorrect'}, status=401)
+
+    if not user.is_active:
+        return JsonResponse({'status': 'error', 'message': 'Account is disabled'}, status=401)
+
+    login(request, user)
+    profile = Profile.objects.filter(user=user).first()
+    if profile is None:
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Login successful, but no profile found',
+            'data': {'address': user.wallet_address}
+        }, status=200)
+
+    serializer = ProfileDetailSerializer(profile, context={'request': request})
+    return JsonResponse({
+        'status': 'success', 
+        'message': 'Login successful',
+        'data': serializer.data
+    }, status=200)

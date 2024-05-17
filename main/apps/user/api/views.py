17bc96dc191df7c39f7eddf0f1939ca43a17bf6d
brianhsieh01 <chyuanxie@gmail.com>
2024-05-17 02:11:06 +0000

    feat: update login api response

--- main/apps/user/api/views.py
+++ main/apps/user/api/views.py
@@ -20,7 +20,7 @@ def login_view(request):
         return JsonResponse({
             'status': 'success',
             'message': 'User created and logged in successfully',
-            'data': {'address': new_user.wallet_address}
+            'data': {'address': new_user.wallet_address, 'email': new_user.email}
         }, status=200)
 
     user = authenticate(request, username=email, password=password)
@@ -36,12 +36,12 @@ def login_view(request):
         return JsonResponse({
             'status': 'success',
             'message': 'Login successful, but no profile found',
-            'data': {'address': user.wallet_address}
+            'data': {'address': user.wallet_address, 'email': user.email}
         }, status=200)
 
     serializer = ProfileDetailSerializer(profile, context={'request': request})
     return JsonResponse({
         'status': 'success', 
         'message': 'Login successful',
-        'data': serializer.data
+        'data':{'address': serializer.data.get("address"), 'email': serializer.data.get("email")}
     }, status=200)
